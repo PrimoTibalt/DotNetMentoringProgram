d50041aff66095f5566d21721812ef5612b5cadf
= <anton.shheglov.1@gmail.com>
2021-03-29 01:37:12 +0300

    add: week 1 wpf app.

---
+++ BaseSolution/Week1WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using WeekFirstConsoleApp.Validators;
+
+namespace Week1WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var name = InputText.Text;
+            var validator = new NameValidator();
+            if (validator.Validate(name))
+            {
+                OutputText.Content = $"Hello, {name}!";
+            }
+            else
+            {
+                OutputText.Content = "Name should be without white spaces, digits or special symbols. Start the name with upper case symbol. Try again...";
+            }
+        }
+    }
+}
