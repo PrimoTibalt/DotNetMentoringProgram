28f27e862ca0788e08c210b972923d0fb089846f
= <anton.shheglov.1@gmail.com>
2021-05-03 22:26:00 +0300

    add: tests on null and empty.

--- ModuleThreeSecondTaskTests/Tests.cs
+++ ModuleThreeSecondTaskTests/Tests.cs
@@ -265,5 +265,148 @@ namespace ModuleThreeSecondTaskTests
 
             Assert.True(list.Count == 1);
         }
+
+        /// <summary>
+        /// Checkes do the visitor returned path that fits filtering.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        [Fact]
+        public async Task GroupOfMethods_ReturnedRightFileOnStop_Returned()
+        {
+            var paths = new List<string>
+            {
+                @"c:\myfile.txt",
+                @"c:\demo\jQuery.js",
+                @"c:\demo\image.gif",
+                @"c:\files\home.txt",
+                @"c:\files\jQuery.js",
+                @"c:\files\sun\image.gif",
+                @"c:\files\sun\aaawifihsdifhish.txt",
+                @"c:\files\zangetsu\motherland.js",
+                @"c:\files\zangetsu\moon\neiborhood.gif",
+                @"c:\files\stand\here\comrad\sunday.txt",
+                @"c:\files\mimik\spider\man\tongue\control\panel\MilesMorales.js",
+                @"c:\files\mimik\spider\man\image.gif",
+                @"c:\files\mimik\spider\man\tongue\control\Gokuden.txt",
+                @"c:\files\mimik\spider\man\tongue\rainbow.js",
+                @"c:\files\mimik\spider\man\tongue\colors\pink.gif",
+                @"c:\files\mirror\clone.txt",
+            };
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { paths[0], new MockFileData("Testing is meh.") },
+                { paths[1], new MockFileData("some js") },
+                { paths[2], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+                { paths[3], new MockFileData("Testing is meh.") },
+                { paths[4], new MockFileData("some js") },
+                { paths[5], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+                { paths[6], new MockFileData("Testing is meh.") },
+                { paths[7], new MockFileData("some js") },
+                { paths[8], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+                { paths[9], new MockFileData("Testing is meh.") },
+                { paths[10], new MockFileData("some js") },
+                { paths[11], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+                { paths[12], new MockFileData("Testing is meh.") },
+                { paths[13], new MockFileData("some js") },
+                { paths[14], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+            });
+            string initialPath = @"c:\";
+            string extension = ".gif";
+            Func<IFileSystemInfo, bool> predicate = (info) => info.Extension == extension;
+            var visitor = new FileSystemVisitor(fileSystem, predicate, initialPath);
+            visitor.FilteredFileFound += (senger, args) => args.Stop = true;
+            var list = new List<string>();
+
+            await foreach (var name in visitor.Search())
+            {
+                list.Add(name);
+            }
+
+            Assert.Contains(extension, list[0]);
+        }
+
+        /// <summary>
+        /// Checkes do the visitor returned path that fits filtering.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        [Fact]
+        public void Method_ErrorOnNullInFileSystem_Throws()
+        {
+            var paths = new List<string>
+            {
+                @"c:\myfile.txt",
+                @"c:\demo\jQuery.js",
+                @"c:\demo\image.gif",
+                @"c:\files\home.txt",
+            };
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { paths[0], new MockFileData("Testing is meh.") },
+                { paths[1], new MockFileData("some js") },
+                { paths[2], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+                { paths[3], new MockFileData("Testing is meh.") },
+            });
+            var initialPath = @"c:\";
+            Assert.Throws<ArgumentNullException>(() => new FileSystemVisitor(null, (info) => true, initialPath));
+        }
+
+        /// <summary>
+        /// Checkes do the visitor returned path that fits filtering.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        [Fact]
+        public async Task GroupOfMethods_NoFilesOnWrongPath_Returned()
+        {
+            var paths = new List<string>
+            {
+                @"c:\myfile.txt",
+                @"c:\demo\jQuery.js",
+                @"c:\demo\image.gif",
+                @"c:\files\home.txt",
+                @"c:\files\jQuery.js",
+                @"c:\files\sun\image.gif",
+                @"c:\files\sun\aaawifihsdifhish.txt",
+                @"c:\files\zangetsu\motherland.js",
+                @"c:\files\zangetsu\moon\neiborhood.gif",
+                @"c:\files\stand\here\comrad\sunday.txt",
+                @"c:\files\mimik\spider\man\tongue\control\panel\MilesMorales.js",
+                @"c:\files\mimik\spider\man\image.gif",
+                @"c:\files\mimik\spider\man\tongue\control\Gokuden.txt",
+                @"c:\files\mimik\spider\man\tongue\rainbow.js",
+                @"c:\files\mimik\spider\man\tongue\colors\pink.gif",
+                @"c:\files\mirror\clone.txt",
+            };
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { paths[0], new MockFileData("Testing is meh.") },
+                { paths[1], new MockFileData("some js") },
+                { paths[2], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+                { paths[3], new MockFileData("Testing is meh.") },
+                { paths[4], new MockFileData("some js") },
+                { paths[5], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+                { paths[6], new MockFileData("Testing is meh.") },
+                { paths[7], new MockFileData("some js") },
+                { paths[8], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+                { paths[9], new MockFileData("Testing is meh.") },
+                { paths[10], new MockFileData("some js") },
+                { paths[11], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+                { paths[12], new MockFileData("Testing is meh.") },
+                { paths[13], new MockFileData("some js") },
+                { paths[14], new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) },
+            });
+            string initialPath = @"c:\monkey";
+            string extension = ".gif";
+            Func<IFileSystemInfo, bool> predicate = (info) => info.Extension == extension;
+            var visitor = new FileSystemVisitor(fileSystem, predicate, initialPath);
+            visitor.FilteredFileFound += (senger, args) => args.Stop = true;
+            var list = new List<string>();
+
+            await foreach (var name in visitor.Search())
+            {
+                list.Add(name);
+            }
+
+            Assert.Empty(list);
+        }
     }
 }
