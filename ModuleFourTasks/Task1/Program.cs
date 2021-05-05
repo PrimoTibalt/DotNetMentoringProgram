9339d50508919b19e6ce9f9d1cdb45b39451a240
= <anton.shheglov.1@gmail.com>
2021-05-05 23:55:46 +0300

    add: solution for first task of exception handling module.

--- ModuleFourTasks/Task1/Program.cs
+++ ModuleFourTasks/Task1/Program.cs
@@ -7,7 +7,19 @@ namespace Task1
     {
         private static void Main(string[] args)
         {
+            const string finishWords = @"Click any button to close application.";
+            if (args.Length < 1)
+            {
+                Console.WriteLine("No arguments were provided. Try again...");
+            }
+            else
+            {
+                args.ToList().ForEach((item) => Console.Write(item[0]));
+                Console.WriteLine();
+            }
 
+            Console.WriteLine(finishWords);
+            Console.ReadKey();
         }
     }
 }
