9a93b0f2bb6fcbc4d93435685ee6a63f2519eadc
= <anton.shheglov.1@gmail.com>
2021-05-05 07:31:00 +0300

    add: first task.

---
+++ ModuleFourTasks/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string finishWords = @"Click any button to close application.";
+            if (args.Length < 1)
+            {
+                Console.WriteLine("No arguments were provided. Try again...");
+            }
+            else
+            {
+                args.ToList().ForEach((item) => Console.Write(item[0]));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(finishWords);
+            Console.ReadKey();
+        }
+    }
+}
