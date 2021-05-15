d71bf5c4982dbafc8aefc3a06fb8a2eac00e9274
= <anton.shheglov.1@gmail.com>
2021-05-15 04:48:44 +0300

    fix: code review issues from linq.

--- ModuleFifthTasks/Task1/LinqTask.cs
+++ ModuleFifthTasks/Task1/LinqTask.cs
@@ -9,6 +9,11 @@ namespace Task1
     {
         public static IEnumerable<Customer> Linq1(IEnumerable<Customer> customers, decimal limit)
         {
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
             return customers.Where(customer => customer.Orders.Sum(order => order.Total) > limit);
         }
 
@@ -17,7 +22,22 @@ namespace Task1
             IEnumerable<Supplier> suppliers
         )
         {
-            return customers.Select((customer, num) => new ValueTuple<Customer, IEnumerable<Supplier>>(customer, suppliers.Where(supplier => supplier.Country == customer.Country && supplier.City == customer.City)));
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
+            if (suppliers is null)
+            {
+                throw new ArgumentNullException(nameof(suppliers));
+            }
+
+            return customers.Select((customer, num) =>
+            (
+                customer,
+                suppliers.Where(supplier => supplier.Country == customer.Country
+                    && supplier.City == customer.City)
+            ));
         }
 
         public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2UsingGroup(
@@ -25,15 +45,35 @@ namespace Task1
             IEnumerable<Supplier> suppliers
         )
         {
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
+            if (suppliers is null)
+            {
+                throw new ArgumentNullException(nameof(suppliers));
+            }
+
             return customers.GroupJoin(
                 suppliers,
                 customer => customer,
                 supplier => new Customer(),
-                (c, s) => new ValueTuple<Customer, IEnumerable<Supplier>>(c, s.Where(supplier => supplier.Country == c.Country && supplier.City == c.City)));
+                (customer, suplier) =>
+                (
+                    customer,
+                    suplier.Where(supplier => supplier.Country == customer.Country
+                        && supplier.City == customer.City)
+                ));
         }
 
         public static IEnumerable<Customer> Linq3(IEnumerable<Customer> customers, decimal limit)
         {
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
             return customers.Where(customer => customer.Orders.Any(order => order.Total >= limit));
         }
 
@@ -41,45 +81,65 @@ namespace Task1
             IEnumerable<Customer> customers
         )
         {
-            return customers.Where(customer => customer.Orders.Count() > 0).Select(customer => new ValueTuple<Customer, DateTime>(customer, customer.Orders.Min(order => order.OrderDate)));
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
+            return customers
+                .Where(customer => customer.Orders.Any())
+                .Select(customer => (customer, customer.Orders.Min(order => order.OrderDate)));
         }
 
         public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq5(
             IEnumerable<Customer> customers
         )
         {
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
             return Linq4(customers)
                 .OrderBy(tuple => tuple.dateOfEntry.Year)
                 .ThenBy(tuple => tuple.dateOfEntry.Month)
-                .ThenByDescending(tuple => tuple.customer.Orders.Sum(order => order.Total));
+                .ThenByDescending(tuple => tuple.customer.Orders.Sum(order => order.Total))
+                .ThenBy(tuple => tuple.customer.CompanyName);
         }
 
         public static IEnumerable<Customer> Linq6(IEnumerable<Customer> customers)
         {
-            return customers.Where(customer => string.IsNullOrWhiteSpace(customer.Region) || string.IsNullOrWhiteSpace(customer.PostalCode) || string.IsNullOrWhiteSpace(customer.Fax) || !customer.Phone.Contains("(") || !customer.PostalCode.Any(ch => char.IsNumber(ch)));
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
+            return customers.Where(customer => string.IsNullOrWhiteSpace(customer.Region) ||
+                string.IsNullOrWhiteSpace(customer.PostalCode) ||
+                string.IsNullOrWhiteSpace(customer.Fax) ||
+                !customer.Phone.Contains("(") ||
+                !customer.PostalCode.Any(ch => char.IsNumber(ch)));
         }
 
         public static IEnumerable<Linq7CategoryGroup> Linq7(IEnumerable<Product> products)
         {
-            return products.GroupBy(product => product.Category).Select(category => new Linq7CategoryGroup { Category = category.Key }).Join(
-                products,
-                selectorInner => selectorInner.Category,
-                selectorOuter => selectorOuter.Category,
-                (category, product) =>
-                {
-                    category.UnitsInStockGroup = category.UnitsInStockGroup ?? new List<Linq7UnitsInStockGroup>();
-                    if (category.UnitsInStockGroup.Any(unit => unit.UnitsInStock == product.UnitsInStock))
-                    {
-                        category.UnitsInStockGroup.FirstOrDefault(unit => unit.UnitsInStock == product.UnitsInStock).Prices.ToList().Add(product.UnitPrice);
-                    }
-                    else
-                    {
-                        category.UnitsInStockGroup.ToList().Add(new Linq7UnitsInStockGroup { UnitsInStock = product.UnitsInStock, Prices = new List<decimal> { product.UnitPrice } });
-                    }
+            if (products is null)
+            {
+                throw new ArgumentNullException(nameof(products));
+            }
 
-                    category.UnitsInStockGroup.ToList().Add(new Linq7UnitsInStockGroup { Prices = new HashSet<decimal> { product.UnitPrice }, UnitsInStock = product.UnitsInStock });
-                    return category;
-                });
+            return products.GroupBy(product => product.Category, (category, products) => new Linq7CategoryGroup
+            {
+                Category = category,
+                UnitsInStockGroup = products.GroupBy(
+                    product => product.UnitsInStock,
+                    product => product.UnitPrice,
+                    (unitsInStock, groupedProducts) => new Linq7UnitsInStockGroup
+                    {
+                        UnitsInStock = unitsInStock,
+                        Prices = groupedProducts.OrderBy(price => price)
+                    })
+            });
         }
 
         public static IEnumerable<(decimal category, IEnumerable<Product> products)> Linq8(
@@ -89,30 +149,62 @@ namespace Task1
             decimal expensive
         )
         {
-            return new List<ValueTuple<decimal, IEnumerable<Product>>>
+            if (products is null)
             {
-                new ValueTuple<decimal, IEnumerable<Product>>(cheap, new List<Product>(products.Where(product => product.UnitPrice <= cheap))),
-                new ValueTuple<decimal, IEnumerable<Product>>(middle, new List<Product>(products.Where(product => product.UnitPrice <= middle && product.UnitPrice > cheap))),
-                new ValueTuple<decimal, IEnumerable<Product>>(expensive, new List<Product>(products.Where(product => product.UnitPrice > middle))),
-            };
+                throw new ArgumentNullException(nameof(products));
+            }
+
+            if (cheap > middle)
+            {
+                throw new ArgumentException(nameof(middle));
+            }
+
+            if (middle > expensive)
+            {
+                throw new ArgumentException(nameof(expensive));
+            }
+
+            return products.GroupBy(product =>
+                    product.UnitPrice <= cheap ? cheap :
+                    product.UnitPrice <= middle && product.UnitPrice > cheap ? middle :
+                    expensive).Select(group => new ValueTuple<decimal, IEnumerable<Product>>(group.Key, group));
         }
 
         public static IEnumerable<(string city, int averageIncome, int averageIntensity)> Linq9(
             IEnumerable<Customer> customers
         )
         {
+            if (customers is null)
+            {
+                throw new ArgumentNullException(nameof(customers));
+            }
+
             return customers.GroupBy(customer => customer.City).Select(category =>
             {
-                return new ValueTuple<string, int, int>(
+                return (
                     category.Key,
-                    Convert.ToInt32(customers.Where(customer => customer.City == category.Key).Average(customer => customer.Orders.Any() ? customer.Orders.Sum(order => order.Total) : 0)),
-                    (int)customers.Where(customer => customer.City == category.Key).Average(customer => customer.Orders.Count()));
+                    Convert.ToInt32(customers
+                        .Where(customer => customer.City == category.Key)
+                        .Average(customer => customer.Orders.Any() ? customer.Orders.Sum(order => order.Total) : 0)),
+                    (int)customers
+                        .Where(customer => customer.City == category.Key)
+                        .Average(customer => customer.Orders.Count()));
             });
         }
 
         public static string Linq10(IEnumerable<Supplier> suppliers)
         {
-            return string.Join(string.Empty, suppliers.OrderBy(supplier => supplier.Country.Length).ThenBy(supplier => supplier.Country).GroupBy(supplier => supplier.Country).Select(supplier => supplier.Key));
+            if (suppliers is null)
+            {
+                throw new ArgumentNullException(nameof(suppliers));
+            }
+
+            return string.Join(
+                string.Empty,
+                suppliers.OrderBy(supplier => supplier.Country.Length)
+                    .ThenBy(supplier => supplier.Country)
+                    .GroupBy(supplier => supplier.Country)
+                    .Select(supplier => supplier.Key));
         }
     }
 }
